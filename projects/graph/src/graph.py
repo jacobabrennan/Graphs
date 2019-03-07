ebfd8bc4a8e8e71a52ce4688435cf3d355781743
JacobABrennan <jacobabrennan@gmail.com>
2019-03-07 12:42:56 -0500

    Clean and merge

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -30,7 +30,7 @@ class Graph:
         vertex_end = self.vertices[end_id]
         vertex_end.add(source_id)
 
-    def traverse_breath_first(self, start_id):
+    def traverse_breadth_first(self, start_id):
         """Prints a path of vertices visited, starting at a given vertex."""
         path = []
         # Define the open and closed vertices trackers
@@ -75,3 +75,62 @@ class Graph:
                 open_vertices.append(linked_id)
         # Print path
         print(F'Path: {path}')
+
+    def search_breadth_first(self, id_start, id_end):
+        path = []
+        # Define the open and closed vertices trackers
+        vertices_open = Queue()
+        vertices_closed = set()
+        # Iterate over each vertex
+        vertices_open.put(id_start)
+        while not vertices_open.empty():
+            # Skip nodes already visited
+            id_current = vertices_open.get()
+            if id_current in vertices_closed:
+                continue
+            # Add each vertex to the path
+            path.append(id_current)
+            vertices_closed.add(id_current)
+            # Check if end vertex has been reached
+            current_vertex = self.vertices[id_current]
+            if id_end in current_vertex:
+                path.append(id_end)
+                break
+            # Queue-up linked vertices
+            for id_linked in current_vertex:
+                vertices_open.put(id_linked)
+    
+    def bfs(self, starting_vertex_id, target_id):
+        q = Queue()
+        visited = set()
+        q.enqueue([starting_vertex_id])
+        while q.size() > 0:
+            path = q.dequeue()
+            v = path[-1]
+            if v not in visited:
+                visited.add(v)
+                if v == target_id:
+                    return path
+                for neighbor in self.verticies[v]:
+                    new_path = list(path)
+                    new_path.append(neighbor)
+                    q.enqueue(new_path)
+
+test_graph = Graph()
+test_graph.add_vertex('A')
+test_graph.add_vertex('B')
+test_graph.add_vertex('C')
+test_graph.add_vertex('D')
+test_graph.add_vertex('E')
+test_graph.add_vertex('F')
+test_graph.add_vertex('G')
+test_graph.add_edge('A', 'A')
+test_graph.add_edge('A', 'B')
+test_graph.add_edge('B', 'C')
+test_graph.add_edge('B', 'D')
+test_graph.add_edge('B', 'E')
+test_graph.add_edge('C', 'E')
+test_graph.add_edge('D', 'E')
+test_graph.add_edge('D', 'G')
+test_graph.add_edge('E', 'F')
+test_graph.traverse_depth_first_stack('C')
